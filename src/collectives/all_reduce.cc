f1ba2fc274208193ab255096b3b6242c11b36ffd
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2020-10-14 12:57:29 +0200

    remove outdated comments

--- src/collectives/all_reduce.cc
+++ src/collectives/all_reduce.cc
@@ -10,12 +10,8 @@ NCCL_API(ncclResult_t, ncclAllReduce, const void* sendbuff, void* recvbuff, void
     ncclDataType_t datatype, ncclRedOp_t op, ncclComm* comm, cudaStream_t stream);
 ncclResult_t ncclAllReduce(const void* sendbuff, void* recvbuff, void* tempbuff1, void* tempbuff2, size_t count,
     ncclDataType_t datatype, ncclRedOp_t op, ncclComm* comm, cudaStream_t stream) {
-
-  //void* tempbuff = malloc(sizeof(recvbuff) * sizeof(recvbuff(0)));
   struct ncclInfo info = { ncclCollAllReduce, "AllReduce",
     sendbuff, recvbuff, tempbuff1, tempbuff2, count, datatype, op, 0, comm, stream, /* Args */
     ALLREDUCE_CHUNKSTEPS, ALLREDUCE_SLICESTEPS};
-  //size_t  heapSize = 1024 * 1024 * 1024;
-  //cudaDeviceSetLimit(cudaLimitMallocHeapSize, heapSize);
   return ncclEnqueueCheck(&info);
 }
